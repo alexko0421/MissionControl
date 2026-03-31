de8539c2244b74fe59fa18542d8bdf93decf71e4
Ko Chunlong <kochunlong@KodeMacBook-Pro.lan>
2026-03-31 16:08:28 -0700

    feat: localize menu bar and add Open Source About panel

--- MissionControl/MissionControlApp.swift
+++ MissionControl/MissionControlApp.swift
@@ -16,6 +16,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private var panel: FloatingPanel!
     private var store = AgentStore()
     private var globalMonitor: Any?
+    private var statusItem: NSStatusItem?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         // Create the floating panel
@@ -44,8 +45,50 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Hide dock icon
         NSApp.setActivationPolicy(.accessory)
 
+        // Create Status Bar Icon
+        setupStatusBar()
+
         // Register global hotkey
         registerGlobalHotkey()
+    }
+
+    private func setupStatusBar() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let button = statusItem?.button {
+            button.image = NSImage(systemImageName: "target")
+            button.toolTip = "MissionControl"
+        }
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "显示主面板 (Show Dashboard)", action: #selector(showDashboard), keyEquivalent: "m"))
+        menu.addItem(NSMenuItem(title: "设置... (Settings)", action: #selector(openSettings), keyEquivalent: ","))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "关于 MissionControl...", action: #selector(showAbout), keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "退出 (Quit)", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        statusItem?.menu = menu
+    }
+
+    @objc private func showDashboard() {
+        togglePanel()
+    }
+
+    @objc private func openSettings() {
+        SettingsWindowController.shared.show()
+    }
+
+    @objc private func showAbout() {
+        NSApp.activate(ignoringOtherApps: true)
+        let options: [NSApplication.AboutPanelOptionKey: Any] = [
+            .applicationName: "MissionControl",
+            .credits: NSAttributedString(
+                string: "GitHub: https://github.com/alexko0421/MissionControl\n开发者: Ko Chunlong\n开源授权: MIT License",
+                attributes: [.font: NSFont.systemFont(ofSize: 12), .foregroundColor: NSColor.labelColor]
+            )
+        ]
+        NSApp.orderFrontStandardAboutPanel(options: options)
     }
 
     func applicationWillTerminate(_ notification: Notification) {
